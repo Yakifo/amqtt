9f35a20b78fd20bfcf8ff4616754170a2311c5b6
Florian Ludwig <f.ludwig@greyrook.com>
2021-03-14 13:55:17 +0100

    replace not_in_dict_or_none with more pythonic check

--- hbmqtt/utils.py
+++ hbmqtt/utils.py
@@ -10,19 +10,6 @@ import yaml
 logger = logging.getLogger(__name__)
 
 
-def not_in_dict_or_none(dict, key):
-    """
-    Check if a key exists in a map and if it's not None
-    :param dict: map to look for key
-    :param key: key to find
-    :return: true if key is in dict and not None
-    """
-    if key not in dict or dict[key] is None:
-        return True
-    else:
-        return False
-
-
 def format_client_message(session=None, address=None, port=None):
     if session:
         return "(client id=%s)" % session.client_id
