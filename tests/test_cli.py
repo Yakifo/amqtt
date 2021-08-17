20bf631206d3dc0d806efff32e0b6beec2f1a9c6
Florian Ludwig <f.ludwig@greyrook.com>
2021-08-17 16:16:03 +0200

    remove legacy cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,17 +13,3 @@ def test_smometest():
     output = subprocess.check_output(["amqtt_pub", "--help"])
     assert b"Usage" in output
     assert b"amqtt_pub" in output
-
-
-def test_smometest_legacy():
-    output = subprocess.check_output(["hbmqtt", "--help"])
-    assert b"Usage" in output
-    assert b"aMQTT" in output
-
-    output = subprocess.check_output(["hbmqtt_sub", "--help"])
-    assert b"Usage" in output
-    assert b"amqtt_sub" in output
-
-    output = subprocess.check_output(["hbmqtt_pub", "--help"])
-    assert b"Usage" in output
-    assert b"amqtt_pub" in output
