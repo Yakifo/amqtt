436e27a3a43f31160cd9b7c3051f11d940dacbe8
burcutanis <burcutanis@sabanciuniv.edu>
2023-03-18 18:28:35 +0300

    little fix for database connection on the broker_start_mysql2.py

--- samples/broker_start_mysql2.py
+++ samples/broker_start_mysql2.py
@@ -40,9 +40,63 @@ config = {
 
 broker = Broker(config)  
 
+
+async def startBroker():
+    await broker.start()
+
 @asyncio.coroutine
-def startBroker():
-    yield from broker.start()
+def connectDB():
+    mydb = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password=""
+        ) 
+    mycursor = mydb.cursor()
+
+    try:
+        mycursor.execute("CREATE DATABASE deneme")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
+            print("database already exists.")
+        else:
+            print("Create DB error ", err)
+
+
+    try:
+        mycursor.execute("USE {}".format("deneme"))
+    except Exception as e:
+        print(e.args)
+    
+    incomingmessages = (
+        "CREATE TABLE `incomingmessages` ("
+        "  `client_id` varchar(20) NOT NULL,"
+        "  `topic` varchar(50) NOT NULL,"
+        "  `message` varchar(100) NOT NULL,"
+        "  `received_date` date NOT NULL,"
+        "  PRIMARY KEY (`client_id`,`received_date`)"
+    ") ENGINE=InnoDB")
+            
+    
+    print("Trying to create table {}: ".format(incomingmessages), end='')
+    try:
+        mycursor.execute(incomingmessages)
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("table already exists.")
+        else:
+            print("Create table error ", err)
+
+    today = date.today()
+    print("will call push data")
+    try:
+        pushDataTodatabase("dummy2", "topic/subtopic", "dummy message", today)
+        pushDataTodatabase("dummy3", "topic/subtopic", "dummy message", today)
+        pushDataTodatabase("dummy4", "topic/subtopic", "dummy message", today)
+    except Exception as exc:
+        print("exception trhown when pushing dummy data to database")
+        print(exc.args)
+
 
 
 def pushDataTodatabase(cli_id, topic, mesg, recevied_at):
@@ -80,100 +134,28 @@ def pushDataTodatabase(cli_id, topic, mesg, recevied_at):
         print("Failed pushing data: {}".format(err))
 
 
-@asyncio.coroutine
-def connectToDatabaseAndAddTable():
-    print("in connectToDatabaseAndAddTable")
-
-    mydb = mysql.connector.connect(
-                host="127.0.0.1",
-                user="root",
-                password=""
-            )
-
-    mycursor = mydb.cursor()
-
-    try:
-        mycursor.execute("create database deneme")
-    except mysql.connector.Error as err:
-
-        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
-            print("database already exists.")
-
-            try:
-                mycursor.execute("USE {}".format("deneme"))
-            except Exception as e:
-                print(e.args)
-
-            TABLES = {}
-            TABLES['incomingmessages'] = (
-                "CREATE TABLE `incomingmessages` ("
-                "  `client_id` varchar(20) NOT NULL,"
-                "  `topic` varchar(50) NOT NULL,"
-                "  `message` varchar(100) NOT NULL,"
-                "  `received_date` date NOT NULL,"
-                "  PRIMARY KEY (`client_id`,`received_date`)"
-                ") ENGINE=InnoDB")
-            
-            for table_name in TABLES:
-                table_description = TABLES[table_name]
-                
-                print("Trying to create table {}: ".format(table_name), end='')
-                try:
-                    mycursor.execute(table_description)
-
-                except mysql.connector.Error as err:
-                    if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                        print("table already exists.")
-
-
-
-                        #calling push data for demo purposes
-
-                        today = date.today()
-                        print("will call push data")
-                        try:
-                            pushDataTodatabase("dummy2", "topic/subtopic", "dummy message", today)
-                            pushDataTodatabase("dummy3", "topic/subtopic", "dummy message", today)
-                            pushDataTodatabase("dummy4", "topic/subtopic", "dummy message", today)
-                        except Exception as exc:
-                            print("exception trhown when pushing dummy data to database")
-                            print(exc.args)
-                    else:
-                        print(err.msg)
-                else:
-                    print("OK")
-
-        else:
-            print("Failed creating database: {}".format(err))
-            exit(1)
-
-@asyncio.coroutine
-def brokerGetMessage(): #for getting message from publisher
+async def brokerGetMessage(): #for getting message from publisher
     C = MQTTClient()
-    yield from C.connect('mqtt://localhost:1883/')
-    yield from C.subscribe([
+    await C.connect('mqtt://localhost:1883/')
+    await C.subscribe([
         ("topic/test", QOS_1)
     ])
     logger.info('Subscribed!')
     try:
         for i in range(1,100):
-            message = yield from C.deliver_message()
+            message = await C.deliver_message()
 
             packet = message.publish_packet
-            print("packet var")
             print(packet.payload.data.decode('utf-8'))
-            
-
             #add a way to access the client id
     except ClientException as ce:
         logger.error("Client exception : %s" % ce)
 
 if __name__ == "__main__":   #it means when this broker_start.py executed, it will run
     formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"  #formatter for our logging information
-    # formatter = "%(asctime)s :: %(levelname)s :: %(message)s"
     #logging.basicConfig(level=logging.INFO, format=formatter)
     logging.basicConfig(level=logging.INFO, format=formatter)
     asyncio.get_event_loop().run_until_complete(startBroker())
-    asyncio.get_event_loop().run_until_complete(connectToDatabaseAndAddTable())
+    asyncio.get_event_loop().run_until_complete(connectDB())
     asyncio.get_event_loop().run_until_complete(brokerGetMessage())
     asyncio.get_event_loop().run_forever()
