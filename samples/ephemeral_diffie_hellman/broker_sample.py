f07dcc3758cc20fbef94adf09e98de4780fe4e9a
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2023-03-22 17:44:14 +0300

    two files addeds for df key exchange
    not completed, will be improved

---
+++ samples/ephemeral_diffie_hellman/broker_sample.py
@@ -0,0 +1,20 @@
+
+
+
+#multi listen
+import sys
+import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+
+# ...
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
