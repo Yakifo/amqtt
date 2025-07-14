07cca7c4db39b63f3487d9991c24d2ea3a6dc429
Andrew Mirsky <andrew@mirsky.net>
2025-07-14 11:49:16 -0400

    Yakifo/amqtt#259 : db schema (orm) and authentication of users plugin, initial

---
+++ amqtt/contrib/db.py
@@ -0,0 +1,154 @@
+from dataclasses import dataclass, field
+import logging
+
+from passlib.context import CryptContext
+from sqlalchemy import String, select
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from amqtt.broker import BrokerContext
+from amqtt.errors import MQTTError
+from amqtt.plugins.base import BaseAuthPlugin
+from amqtt.session import Session
+
+logger = logging.getLogger(__name__)
+
+class Base(DeclarativeBase):
+    pass
+
+
+class PasswordHasher:
+    """singleton to initialize the CryptContext and then use it elsewhere in the code."""
+
+    _instance: "PasswordHasher" = None
+
+    def __init__(self):
+        self._crypt_context = CryptContext()
+
+    def __new__(cls, *args: list, **kwargs: dict) -> "PasswordHasher":
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    @property
+    def crypt_context(self) -> "CryptContext":
+        return self._crypt_context
+
+    @crypt_context.setter
+    def crypt_context(self, value: "CryptContext") -> None:
+        self._crypt_context = value
+
+class UserAuth(Base):
+    __tablename__ = "user_auth"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True)
+    _password_hash: Mapped[str] = mapped_column("password_hash", String(128))
+
+    @hybrid_property
+    def password(self):
+        raise AttributeError("Password is write-only")
+
+    @password.setter
+    def password(self, plain_password: str):
+        self._password_hash = PasswordHasher().crypt_context.hash(plain_password)
+
+    def verify_password(self, plain_password: str) -> bool:
+        return PasswordHasher().crypt_context.verify(plain_password, self._password_hash)
+
+
+
+def default_hash_scheme():
+    return ["argon2", "bcrypt", "pbkdf2_sha256", "scrypt"]
+
+
+class DBAuthPlugin(BaseAuthPlugin):
+
+    def __init__(self, context: BrokerContext):
+        super().__init__(context)
+
+        # access the singleton and set the proper crypt context
+        pwd_hasher = PasswordHasher()
+        pwd_hasher.crypt_context = CryptContext(schemes=self.config.hash_schemes, deprecated="auto")
+
+        self._engine = create_async_engine(f"{self.config.connection}")
+        self._db_session_maker = async_sessionmaker(self._engine, expire_on_commit=False)
+
+    async def on_broker_pre_start(self) -> None:
+        """Sync the schema (if configured)"""
+        if not self.config.sync_schema:
+            return
+        async with self._engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    async def authenticate(self, *, session: Session) -> bool | None:
+
+        if not session.username:
+            return False
+
+        async with self._db_session_maker() as db_session, db_session.begin():
+
+            stmt = select(UserAuth).filter(UserAuth.username == session.username)
+            user_auth = await db_session.scalar(stmt)
+            if not user_auth:
+                logger.info(f"Username '{session.username}' does not exist.")
+                return False
+
+            await db_session.flush()
+            if not user_auth.verify_password(session.password):
+                logger.info(f"Username '{session.username}' password mismatch.")
+                return False
+            return True
+
+
+    @dataclass
+    class Config:
+        """Configuration for DB authentication."""
+
+        connection: str
+        """SQLAlchemy connection string for the asyncio version of the database connector:
+        - mysql+aiomysql://user:password@host:port/dbname
+        - postgresql+asyncpg://user:password@host:port/dbname
+        - sqlite+aiosqlite:///dbfilename.db
+        """
+        sync_schema: bool = False
+        """Use SQLAlchemy to create / update the database schema."""
+        hash_schemes: list[str] = field(default_factory=default_hash_scheme)
+        """An ordered list of which algorithms to hash and verify passwords."""
+
+
+class UserManager:
+
+    def __init__(self, connection: str):
+        self._engine = create_async_engine(connection)
+        self._db_session_maker = async_sessionmaker(self._engine, expire_on_commit=False)
+
+    async def create_user(self, username: str, plain_password:str) -> UserAuth | None:
+        async with self._db_session_maker() as db_session, db_session.begin():
+            stmt = select(UserAuth).filter(UserAuth.username == username)
+            user_auth = await db_session.scalar(stmt)
+            if user_auth:
+                logger.info(f"Username '{username}' already exists.")
+                raise MQTTError("Username already exists.")
+
+            user_auth = UserAuth(username=username)
+            user_auth.password = plain_password
+            await db_session.flush()
+            return user_auth
+
+        return None
+
+    async def update_password(self, username: str, plain_password: str) -> UserAuth | None:
+        async with self._db_session_maker() as db_session, db_session.begin():
+            stmt = select(UserAuth).filter(UserAuth.username == username)
+            user_auth = await db_session.scalar(stmt)
+            if user_auth:
+                logger.info(f"Username '{username}' already exists.")
+                raise MQTTError("Username already exists.")
+
+            user_auth = UserAuth(username=username)
+            user_auth.password = plain_password
+            await db_session.flush()
+            return user_auth
+        return None
