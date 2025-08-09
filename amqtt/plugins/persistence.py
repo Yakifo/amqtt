4dd5dfe657e673e29846abe3fa6c11943d612f0e
Andrew Mirsky <andrew@mirsky.net>
2025-08-09 14:01:17 -0400

    fixing code comment

--- amqtt/plugins/persistence.py
+++ amqtt/plugins/persistence.py
@@ -8,4 +8,4 @@ class SQLitePlugin(BasePlugin[BrokerContext]):
 
     def __init__(self, context: BrokerContext) -> None:
         super().__init__(context)
-        warnings.warn("SQLitePlugin is deprecated, use amqtt.plugins.persistence.SessionDBPlugin", stacklevel=1)
+        warnings.warn("SQLitePlugin is deprecated, use amqtt.contrib.persistence.SessionDBPlugin", stacklevel=1)
