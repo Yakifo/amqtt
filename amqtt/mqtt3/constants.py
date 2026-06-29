b02681da004699387f004a681fc324927b7cf6ed
Andrew Mirsky <andrew@mirsky.net>
2026-06-29 09:47:23 -0400

    fixed linting errors

--- amqtt/mqtt3/constants.py
+++ amqtt/mqtt3/constants.py
@@ -1,3 +1,7 @@
-from amqtt.constants import QOS_0 as QOS_0, QOS_1 as QOS_1, QOS_2 as QOS_2
+import amqtt.constants as shared_constants
+
+QOS_0 = shared_constants.QOS_0
+QOS_1 = shared_constants.QOS_1
+QOS_2 = shared_constants.QOS_2
 
 __all__ = ["QOS_0", "QOS_1", "QOS_2"]
